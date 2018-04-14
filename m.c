13c7d53d43aa8ae8192a3d2ca97c2a1a4625ac2e
FrowntownPitt <ajf93@pitt.edu>
2018-04-13 22:06:26 -0400

    Made mem test easier to add string

--- m.c
+++ m.c
@@ -12,6 +12,8 @@ main(int argc, char *argv[])
 
 	// args: ./m <pages> <request 1> <reqeust 2>...
 
+	char args[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27};
+
 	if(argc == 1){
 
 	} else {
@@ -20,21 +22,21 @@ main(int argc, char *argv[])
 
 		int * mem = (int *)sbrk(numPages);
 
-		char * num = argv[2]-1;
+		//char * num = argv[2]-1;
 
 		int i=0;
-		for(i=0; num > 0; i++){
+		for(i=0; i<sizeof(args)/sizeof(args[0]); i++){
 			
 
 			//printf(1, "argv[2] loc: 0x%x\n", &argv[2]);
 			//printf(1, "First num loc: 0x%x\n", num);
 
-			printf(1, "num[%d]: %d\n", i, atoi((num+1)));
-			int page = atoi(num+1);
+			printf(1, "num[%d]: %d\n", i, args[i]);
+			int page = args[i];//atoi(num+1);
 
 			mem[(page-1)*PGSIZE/sizeof(int)] = i;
 			
-			num = strchr(num+1, ',');
+			//num = strchr(num+1, ',');
 		}
 
 		sbrk(-numPages);
